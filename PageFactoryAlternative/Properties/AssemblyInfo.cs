fde32aea5c6653e304852cc09e81f913a9c93170
priyav <priyav@TAG212>
2020-02-26 19:16:13 +0530

    Add project files.

---
+++ PageFactoryAlternative/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PageFactoryAlternative")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PageFactoryAlternative")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c4b3a65d-85f5-4a6d-bb94-56d160bac336")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PageFactoryAlternative/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace PageFactoryAlternative
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+
+        private IWebElement fooElement
+        {
+            get
+            {
+                return driver.FindElement(By.Id("foo"));
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            fooElement.Click();            
+        }
+    }
+}
